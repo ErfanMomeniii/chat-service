91acf7cd9eda1b2680dab4a0ab25cf1126a0f89e
Erfan Momeni <Erfan.Momeni@snapp.cab>
2022-12-10 22:00:31 +0330

    add driver interface

--- internal/db/mysql.go
+++ internal/db/mysql.go
@@ -4,61 +4,75 @@ import (
 	"database/sql"
 	"fmt"
 	"github.com/ErfanMomeniii/chat-service/internal/config"
-
 	"github.com/ErfanMomeniii/chat-service/internal/log"
-
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-type Initialize interface {
-	CreateDatabase() (*gorm.DB, error)
-	Migrate(db *gorm.DB) error
+type Driver interface {
+	Connection
+	Register
+	Migration
+	Rollback
+}
+
+type Connection interface {
+	Connect(dbname string) (*gorm.DB, error)
+}
+
+type Register interface {
+	Create(dbname string) (*gorm.DB, error)
+}
+
+type Migration interface {
+	Migrate(db *gorm.DB, models ...interface{}) error
+}
+
+type Rollback interface {
+	Rollback(db *gorm.DB, models ...interface{}) error
 }
 
 type Mysql struct{}
 
-func (m *Mysql) Migrate(db *gorm.DB) error {
+func (m *Mysql) Connect(dbname string) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=True&loc=Local",
+		config.C.Mysql.Username, config.C.Mysql.Password, config.C.Mysql.Host, config.C.Mysql.Port, dbname, config.C.Mysql.Charset)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	return db, err
+}
+
+func (m *Mysql) Create(dbname string) error {
+	conf := fmt.Sprintf("%s:%s@tcp(%s:%s)/",
+		config.C.Mysql.Username, config.C.Mysql.Password, config.C.Mysql.Host, config.C.Mysql.Port)
+
+	db, err := sql.Open("mysql", conf)
+	if err != nil {
+		log.Logger.Fatal(err.Error())
+	}
+
+	_, err = db.Exec("CREATE DATABASE " + dbname)
+	if err != nil {
+		log.Logger.Fatal(err.Error())
+	}
+
 	return nil
 }
 
-func (m *Mysql) CreateDatabase() (*gorm.DB, error) {
-	dbconnect := fmt.Sprintf("%s:%s@tcp(%s:%s)/",
-		config.C.Mysql.Username, config.C.Mysql.Password, config.C.Mysql.Host, config.C.Mysql.Port)
-	sqldb, err := sql.Open("mysql", dbconnect)
-
+func (m *Mysql) Migrate(db *gorm.DB, models ...interface{}) error {
+	err := db.Migrator().AutoMigrate(models)
 	if err != nil {
-		log.Logger.Fatal(err.Error())
+		return err
 	}
 
-	_, err = sqldb.Exec("CREATE DATABASE " + config.C.Mysql.Dbname)
-
-	if err != nil {
-		log.Logger.Fatal(err.Error())
-	}
-
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/authform?charset=%s&parseTime=True&loc=Local",
-		config.C.Mysql.Username, config.C.Mysql.Password, config.C.Mysql.Host, config.C.Mysql.Port, config.C.Mysql.Charset)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
-	if err != nil {
-		return db, err
-	}
-
-	m.Migrate(db)
-
-	return db, nil
+	return nil
 }
 
-func Withretry(createDB func() (*gorm.DB, error), limitAttempt int) *gorm.DB {
-	for i := 0; i < limitAttempt; i++ {
-		db, err := createDB()
-
-		if err != nil {
-			log.Logger.Info(err.Error())
-		}
-
-		return db
+func (m *Mysql) Rollback(db *gorm.DB, models ...interface{}) error {
+	err := db.Migrator().DropTable(models)
+	if err != nil {
+		return err
 	}
 
 	return nil
